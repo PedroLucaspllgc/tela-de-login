b66d0f3218782fb492dad8271a0ca715dbeac035
PedroLucaspllgc <161877989+PedroLucaspllgc@users.noreply.github.com>
2024-05-13 15:41:57 -0300

    Update Registrar.py

--- Registrar.py
+++ Registrar.py
@@ -1,22 +1,23 @@
-from kivy.app import App
+from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
-from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
-from kivy.uix.image import Image
+from kivy.uix.image import AsyncImage
 from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
 
-class Registro(App):
-    def build(self):
+class RegistroScreen(Screen):
+    def __init__(self, **kwargs):
+      super(RegistroScreen, self).__init__(**kwargs)
+
       layout_float = FloatLayout()
       
-      imagem = Image(source='/Users/aluno.sesipaulista/Downloads/ola.png', pos_hint={'x': 0, 'y': 0.2})
+      imagem = AsyncImage(source='https://pt.pngtree.com/freepng/avatar-icon-profile-icon-member-login-vector-isolated_5247852.html', pos_hint={'x': 0, 'y': 0.2})
       
       email = TextInput(text='Email', size_hint=(None, None), size=(450, 50), pos_hint={'x': 0.27, 'y': 0.4})
       senha = TextInput(text='Senha', size_hint=(None, None), size=(450, 50), pos_hint={'x': 0.27, 'y': 0.3})
       
       btn2 = Button(text='Registrar', size_hint=(None, None), size=(450, 50), pos_hint={'x': 0.27, 'y': 0.2}, background_color=get_color_from_hex('#D1D1D1'))
-      btn1 = Button(text='Login', size_hint=(None, None), size=(450, 50), pos_hint={'x': 0.27, 'y': 0.1}, background_color=get_color_from_hex('#D1D1D1'))
       
       layout_float.add_widget(email)
       layout_float.add_widget(senha)
@@ -24,9 +25,5 @@ class Registro(App):
       layout_float.add_widget(btn2)
       
       Window.clearcolor = (1, 1, 1, 1)
-      
-      return layout_float  
 
-      
-if __name__ == "__main__":
-    Registro().run()
+      self.add_widget(layout_float)
